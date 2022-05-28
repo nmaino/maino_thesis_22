6ff23d16581f93f2a0f8141a815793c6028b2403
Nicola Maino <nicola.maino96@gmail.com>
2022-05-28 17:12:48 +0200

    JenaRDFToRDBTripleStoreConverter begun, that is the Jena version of the original RDFToRDBTripleStoreConverter

---
+++ src/main/java/it/unipd/dei/ims/rum/convertion/JenaRDFToRDBTripleStoreConverter.java
@@ -0,0 +1,62 @@
+package it.unipd.dei.ims.rum.convertion;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.tdb.TDBFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * This class is a prototype of a substituting class for
+ * @link{RDFToRDBTripleStoreConverter} originally developed
+ * by Dennis Dosso utilising Blazegraph.
+ * This class makes use of Jena instead.
+ *
+ * @author Nicola Maino
+ */
+public class JenaRDFToRDBTripleStoreConverter {
+
+    public void importFromN3ToDiskDataset(String n3FilePath, String outputDatasetPath)
+            throws FileNotFoundException {
+
+        // To read the file as a stream
+        FileInputStream fin;
+
+        // Opens file and handles exceptions
+        try {
+            fin = new FileInputStream(n3FilePath);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("Cannot open the provided file.\n");
+        }
+
+        // Creates a Dataset instance
+        Dataset dataset =
+                TDBFactory.createDataset(outputDatasetPath);
+
+        // Sets dataset on write mode
+        dataset.begin(ReadWrite.WRITE);
+
+        // Creates a Jena default graph model and reads data from the file
+        Model model = dataset.getDefaultModel();
+        model.read(fin, null, "N3");
+
+        // Commits and closes
+        dataset.commit();
+        dataset.end();
+    }
+
+    public static void main(String args[]) throws FileNotFoundException {
+
+
+        String filePath = "/Users/Nicola Maino/Documents/RDF_DATASETS/linkedmdb_1m/prova.n3";
+        String rdfDataset = "/Users/Nicola Maino/Documents/RDF_DATASETS/linkedmdb_1m/prova";
+
+        JenaRDFToRDBTripleStoreConverter converter = new JenaRDFToRDBTripleStoreConverter();
+
+        converter.importFromN3ToDiskDataset(filePath, rdfDataset);
+
+        System.out.println("RDF Dataset created.\n");
+    }
+}
