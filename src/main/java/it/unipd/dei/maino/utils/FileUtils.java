d8666f34e309c4c3f7c7a067f9bc6d5507c1cbfb
Nicola Maino <nicola.maino96@gmail.com>
2022-12-04 22:31:34 +0100

    utility class for file deletion added

--- src/main/java/it/unipd/dei/maino/utils/FileUtils.java
+++ src/main/java/it/unipd/dei/maino/utils/FileUtils.java
@@ -1,2 +1,26 @@
-package it.unipd.dei.maino.utils;public class FileUtils {
+package it.unipd.dei.maino.utils;
+
+import java.io.File;
+
+public class FileUtils {
+
+    public static void deleteFolder(File folder) {
+
+        File[] files = folder.listFiles();
+
+        try {
+            if (files != null) {
+                for (File f : files) {
+                    if (f.isDirectory()) {
+                        deleteFolder(f);
+                    } else {
+                        f.delete();
+                    }
+                }
+            }
+        } catch (NullPointerException n) {
+            System.err.println("File to be deleted not found.");
+            n.printStackTrace();
+        }
+    }
 }
